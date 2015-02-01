a75c464c6fb03b76760e53dd1be554b24e340283
Jonathan Howard <j@hovverd.com>
2015-02-01 17:00:55 -0500

    Replaced include guards with pragma once

--- include/utils.h
+++ include/utils.h
@@ -3,14 +3,11 @@
  * created 140131 jonathan howard (j@hovverd.com)
  ******************************************************/
 
-#ifndef __OS_UTILS_H__
-#define __OS_UTILS_H__
+#pragma once
 
 #ifdef DEBUG
   #include <cstdio>
   #define DLOG(format, ...) printf("[DEBUG] " + format, __VA_ARGS__);
 #else
   #define DLOG(...)
-#endif
-
 #endif

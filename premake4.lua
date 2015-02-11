26d2b03810b5fa4f403542bbdfeb60007d0301ef
Jonathan Howard <j@hovverd.com>
2015-02-11 17:13:49 -0500

    cleaned up indentation for premake4 file

--- premake4.lua
+++ premake4.lua
@@ -1,30 +1,30 @@
 solution "os"
-  configurations { "Debug", "Release" }
+  configurations  { "Debug", "Release" }
 
   -- Visual Studio default project
   -- startproject "ostests"
 
-  configurations { "Debug*" }
-    defines { "DEBUG" }
-    flags { "Symbols" }
-    targetsuffix "_d"
-    objdir "build/obj_debug"
-    targetdir "bin/debug"
+  configuration "Debug*" 
+    defines       { "DEBUG" }
+    flags         { "Symbols" }
+    targetsuffix  "_d"
+    objdir        "build/obj_debug"
+    targetdir     "bin/debug"
 
-  configurations { "Release*" }
-    defines { }
-    flags { "Optimize" }
-    objdir "build/obj_release"
-    targetdir "bin/release"
+  configuration "Release*"
+    defines       { }
+    flags         { "Optimize" }
+    objdir        "build/obj_release"
+    targetdir     "bin/release"
 
   project "ostests"
-    kind "ConsoleApp"
-    language "C++"
-    files { "src/**.cpp", "test/**.cpp" }
-    excludes { "src/driver.cpp" }
-    includedirs { "include/", "lib/googletest/include" }
-    links { "gtest" }
-    flags { "ExtraWarnings" }
+    kind          "ConsoleApp"
+    language      "C++"
+    files         { "src/**.cpp", "test/**.cpp" }
+    excludes      { "src/driver.cpp" }
+    includedirs   { "include/", "lib/googletest/include" }
+    links         { "gtest" }
+    flags         { "ExtraWarnings" }
 
     configuration "gmake"
       buildoptions { "-std=c++11" }
@@ -46,11 +46,11 @@ solution "os"
         flags    { "Optimize" } 
 
   project "os"
-    kind "ConsoleApp"
-    language "C++"
-    files { "src/**.cpp" }
-    includedirs { "include/" }
-    flags { "ExtraWarnings" }
+    kind          "ConsoleApp"
+    language      "C++"
+    files         { "src/**.cpp" }
+    includedirs   { "include/" }
+    flags         { "ExtraWarnings" }
 
     configuration "gmake"
       buildoptions { "-std=c++11" }

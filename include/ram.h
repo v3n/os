c03b300558383288f3cac55f14cc6bacb6753ed5
Jonathan Howard <j@hovverd.com>
2015-02-27 00:20:43 -0500

    Proof of concept RAM implementation, linear allocation strategy, no memory release strategy so far

--- include/ram.h
+++ include/ram.h
@@ -5,11 +5,21 @@
 
 #include "utils.h"
 
+#pragma once
+
 class RAM
 {
 private:
     WORD * buffer;
+
+    WORD * allocatedPtr;
+
 public: 
     RAM();
     ~RAM();
+
+    void * malloc(size_t);
+    void * calloc(size_t, size_t);
+    void * realloc(void *, size_t);
+    void free(void *);
 };
--- src/ram.cpp
+++ src/ram.cpp
@@ -3,12 +3,18 @@
  * created 140204 jonathan howard (j@hovverd.com)
  ******************************************************/
 
+#include <new>
+
 #include "utils.h"
 #include "ram.h"
+
+#define BUFFER_SIZE (1024 * sizeof(WORD))
 
 RAM::RAM()
 {
     buffer = new WORD[1024];
+
+    allocatedPtr = buffer;
 }
 
 RAM::~RAM()
@@ -16,3 +22,34 @@ RAM::~RAM()
     delete[] buffer;
 }
 
+void * RAM::malloc(size_t size)
+{
+    void * ReturnPtr = allocatedPtr;
+    allocatedPtr += size * sizeof(WORD);
+
+    if ( allocatedPtr > (buffer + BUFFER_SIZE) )
+        throw std::bad_alloc();
+    return ReturnPtr;
+}
+
+void * RAM::calloc(size_t number, size_t size)
+{
+    void * ReturnPtr = allocatedPtr;
+    allocatedPtr += (size * number) * sizeof(WORD);
+    if ( allocatedPtr > (buffer + BUFFER_SIZE) )
+        throw std::bad_alloc();
+    return ReturnPtr;
+}
+
+void * RAM::realloc(void * memory, size_t size)
+{
+    this->free(memory);
+    return this->malloc(size);
+}
+
+void RAM::free(void * memory)
+{
+
+}
+
+#undef BUFFER_SIZE

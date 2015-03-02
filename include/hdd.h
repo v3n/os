514b8ba1029bd69e021d329187ad0db0ff3ad833
Jonathan Howard <j@hovverd.com>
2015-03-01 23:43:07 -0500

    Initial hard drive implementation - should be feature complete for phase 1

--- include/hdd.h
+++ include/hdd.h
@@ -9,10 +9,19 @@
 
 #include "utils.h"
 
+ /* O(n) traversal */
+ typedef struct FileSystem
+ {
+     unsigned int id;
+     size_t size;
+ } FileSystem;
+ 
+
 class HDD
 {
 private:
 	WORD * buffer;
+
 public:	
 	HDD();
 	~HDD();
@@ -20,6 +29,9 @@ public:
     /* Allocate zero'd file */
     void * newFile(size_t);
     /* Allocate new file and copy data to it */
-    void * newFile(void *, size_t);
+    void * newFile(unsigned int, void *, size_t);
+
+    /* return pointer to file */
+    FileSystem * findFile(unsigned int);
 };
 
--- src/hdd.cpp
+++ src/hdd.cpp
@@ -4,11 +4,15 @@
  ******************************************************/
 
 #include "hdd.h"
+#include <cstring>
+#include <stdexcept>
+
+#define BUFFER_SIZE (2048 * sizeof(WORD))
 
 HDD::HDD()
 {
-	/* use c++-style memory allocation */
-	buffer = new WORD[1024]();
+	/* use c-style array memory allocation */
+	buffer = new WORD[2048]();
 }
 
 HDD::~HDD()
@@ -16,3 +20,29 @@ HDD::~HDD()
 	delete[] buffer;
 }
 
+void * HDD::newFile(unsigned int id, void * data, size_t size)
+{
+#if DEBUG
+    if (id == 0)
+        throw std::invalid_argument("id must not equal 0");
+#endif
+    FileSystem * fs;
+    for (fs = (FileSystem *)buffer; fs->id == 0; fs += sizeof(FileSystem) + fs->size);
+    fs->id = id;
+    fs->size = size;
+    std::memcpy(fs + sizeof(FileSystem), data, size);
+
+    return fs;
+}
+
+FileSystem * HDD::findFile(unsigned int id)
+{
+    for (FileSystem * fs = (FileSystem *)buffer; fs->id == 0; fs += sizeof(FileSystem) + fs->size)
+    {
+        if (fs->id == id)
+        {
+            return fs;
+        }
+    }
+    return nullptr;
+}

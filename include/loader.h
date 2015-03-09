270d297708e9ae1e327a5b269d06841aa99769b8
Jonathan Howard <j@hovverd.com>
2015-03-08 20:43:26 -0400

    massive loader overhaul, now supports HDD class, properly stores data as words, now uses STL methods

--- include/loader.h
+++ include/loader.h
@@ -1,27 +1,12 @@
 #pragma once
-#include <string>
+
 #include <fstream>
-#include <sstream>
-#include <iostream>
-#include <vector>
-#include "disk.h"
+
+#include "utils.h"
+#include "hdd.h"
+
 class Loader
 {
 public:
-	Loader(Disk &diskName);
-	struct JobBlock{
-		unsigned long id;
-		unsigned long priority;
-		unsigned long wordSize;
-	};
-	struct JobDataBlock{
-		unsigned long inputBufferSize;
-		unsigned long outputBufferSize;
-		unsigned long tempBufferSize;
-	};
-	std::vector<Loader::JobBlock> jobOrder_v();
-	std::string loadTextFile(const std::string &fileName);
-	std::vector<std::string> parseString_v(std::string &fullString, std::string const &token);
-	std::vector<Loader::JobBlock> loadJobBlock(const std::vector<std::string> &str);
-	std::vector<Loader::JobDataBlock> loadJobDataBlock(const std::vector<std::string> &str);
+	Loader(std::ifstream, HDD *);
 };

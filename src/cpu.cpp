85578451763a17745664c6b229a3acb7ded0ed9b
Jonathan Howard <j@hovverd.com>
2015-03-10 00:46:41 -0400

    begun implementing cpu opcodes
    part of #1

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -12,7 +12,7 @@
 #undef R
 #endif
 /* Access register of this CPU */
-#define R(x) ((WORD *)this + x)
+#define R(x) (*((WORD *)this + x))
 
 #define EXTRACT_TYPECODE(x) (0x003 << 30 & x) >> 30
 #define EXTRACT_OPCODE(x)   (0x06F << 24 & x) >> 24
@@ -44,10 +44,14 @@ void CPU::fetch(const WORD * instr)
     program_counter++;
 }
 
-#define CPU_DECODE_CASE(instr) \
-    case instr: \
-        DLOG(stringify(instr) ); \
-        break;
+#define CPU_DECODE_CASE(instr, type) \
+    case instr: { \
+        DLOG( #instr ); \
+        INSTR_##type instruction = *((INSTR_##type *)&(this->instruction));
+    
+#define CPU_BREAK_CASE \
+    break; \
+    }
 
 void * CPU::decode()
 {
@@ -60,37 +64,64 @@ void * CPU::decode()
     switch (EXTRACT_OPCODE(instruction))
     {
         /* Accumulator    */
-        CPU_DECODE_CASE(RD)
-        CPU_DECODE_CASE(WR)
+        CPU_DECODE_CASE(RD,   Register)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(WR,   Register)
+            CPU_BREAK_CASE
         /* Register       */
-        CPU_DECODE_CASE(MOV)
-        CPU_DECODE_CASE(ADD)
-        CPU_DECODE_CASE(SUB)
-        CPU_DECODE_CASE(MUL)
-        CPU_DECODE_CASE(DIV)
-        CPU_DECODE_CASE(AND)
-        CPU_DECODE_CASE(OR)
-        CPU_DECODE_CASE(SLT)
+        CPU_DECODE_CASE(MOV,  Immediate)
+            R(instruction.d_reg) = R(instruction.b_reg); CPU_BREAK_CASE
+        CPU_DECODE_CASE(ADD,  Arithmatic)
+            R(instruction.d_reg) = R(instruction.s_reg_a) + R(instruction.s_reg_b); CPU_BREAK_CASE
+        CPU_DECODE_CASE(SUB,  Arithmatic)
+            R(instruction.d_reg) = R(instruction.s_reg_a) - R(instruction.s_reg_b); CPU_BREAK_CASE
+        CPU_DECODE_CASE(MUL,  Arithmatic)
+            R(instruction.d_reg) = R(instruction.s_reg_a) * R(instruction.s_reg_b); CPU_BREAK_CASE
+        CPU_DECODE_CASE(DIV,  Arithmatic)
+            R(instruction.d_reg) = R(instruction.s_reg_a) / R(instruction.s_reg_b); CPU_BREAK_CASE
+        CPU_DECODE_CASE(AND,  Arithmatic)
+            R(instruction.d_reg) = R(instruction.s_reg_a) & R(instruction.s_reg_b); CPU_BREAK_CASE
+        CPU_DECODE_CASE(OR,   Arithmatic)
+            R(instruction.d_reg) = R(instruction.s_reg_a) | R(instruction.s_reg_b); CPU_BREAK_CASE
+        CPU_DECODE_CASE(SLT,  Arithmatic)
+            R(instruction.d_reg) = (R(instruction.s_reg_a) < R(instruction.s_reg_b)) ? 1 : 0; CPU_BREAK_CASE
         /* Immediate      */
-        CPU_DECODE_CASE(ST)
-        CPU_DECODE_CASE(LW)
-        CPU_DECODE_CASE(MOVI)
-        CPU_DECODE_CASE(ADDI)
-        CPU_DECODE_CASE(MULI)
-        CPU_DECODE_CASE(DIVI)
-        CPU_DECODE_CASE(LDI)
-        CPU_DECODE_CASE(SLTI)
+        CPU_DECODE_CASE(ST,   Register)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(LW,   Register)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(MOVI, Register)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(ADDI, Register)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(MULI, Register)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(DIVI, Register)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(LDI,  Register)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(SLTI, Register)
+            CPU_BREAK_CASE
         /* Flow           */
-        CPU_DECODE_CASE(HLT)
-        CPU_DECODE_CASE(NOP)
-        CPU_DECODE_CASE(JMP)
+        CPU_DECODE_CASE(HLT,  Flow)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(NOP,  Flow)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(JMP,  Flow)
+            CPU_BREAK_CASE
         /* Branch         */
-        CPU_DECODE_CASE(BEQ)
-        CPU_DECODE_CASE(BNE)
-        CPU_DECODE_CASE(BEZ)
-        CPU_DECODE_CASE(BNZ)
-        CPU_DECODE_CASE(BGZ)
-        CPU_DECODE_CASE(BLZ)
+        CPU_DECODE_CASE(BEQ,  Branch)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(BNE,  Branch)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(BEZ,  Branch)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(BNZ,  Branch)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(BGZ,  Branch)
+            CPU_BREAK_CASE
+        CPU_DECODE_CASE(BLZ,  Branch)
+            CPU_BREAK_CASE
         default:
             return nullptr;
             break;

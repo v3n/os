8a65ddaa9d7dea44826e08f193f9f94b275afa3b
Jonathan Howard <j@hovverd.com>
2015-03-10 10:24:57 -0400

    [test] correct size for opcode structs

---
+++ test/cpu_tests.cpp
@@ -0,0 +1,19 @@
+/******************************************************
+ * cpu_tests.cpp - cpu unit tests
+ * created 140204 jonathan howard (j@hovverd.com)
+ ******************************************************/
+
+#include "gtest/gtest.h"
+
+#include "utils.h"
+#include "cpu.h"
+#include "opcodes.h"
+
+TEST(OpCode, IsCorrectSize)
+{
+    ASSERT_TRUE(sizeof(INSTR_Arithmatic) == 4);
+    ASSERT_TRUE(sizeof(INSTR_Register) == 4);
+    ASSERT_TRUE(sizeof(INSTR_Immediate) == 4);
+    ASSERT_TRUE(sizeof(INSTR_Flow) == 4);
+    ASSERT_TRUE(sizeof(INSTR_Branch) == 4);
+}

ef5e6d313cbc1b32b47e319aa2fa284fc4fa0e0f
Will Owens <malmoi112@gmail.com>
2015-03-15 12:17:07 -0400

    Extended scheduler to support multiple CPU's

--- include/scheduler.h
+++ include/scheduler.h
@@ -16,15 +16,20 @@ private:
 	RAM *buffer;
 	std::map<int, PCB> jobs;
 	std::vector<PCB> readyQueue;
+	std::vector<std::vector<PCB>> readyQueues;
 	void Swap(PCB x, PCB y);
 	void SortQueue(std::vector<PCB> &toSort, int left, int right);	
 	void LoadToRAM(PCB toLoad);	
+	int GetShortestQueue();
 
 public:
 	void Enqueue(PCB next);
 	PCB *Peek();
+	PCB *Peek(int i);
 	PCB Dequeue();
+	PCB Dequeue(int i);	//indexing for multi-core system; each queue corresponds to a specific queue
 	Scheduler();
+	Scheduler(int numCores);
 	~Scheduler();
 };
 
--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -10,30 +10,55 @@
 
 Scheduler::Scheduler()
 {
-
+	std::vector<PCB> nextQueue;
+	readyQueues.push_back(nextQueue);
 }
 
 Scheduler::~Scheduler()
 {
+}
 
+Scheduler::Scheduler(int numCores)
+{
+	for (int i = 0; i < numCores; i++)
+	{
+		std::vector<PCB> nextQueue;
+		readyQueues.push_back(nextQueue);
+	}
 }
 
 void Scheduler::Enqueue(PCB next)
 {
-	readyQueue.push_back(next);
-	SortQueue(readyQueue, 0, readyQueue.size() - 1);
+	int nextQueue = GetShortestQueue();
+	readyQueues[nextQueue].push_back(next);
+	SortQueue(readyQueues[nextQueue], 0, readyQueues[nextQueue].size() - 1);
 }
 
 PCB *Scheduler::Peek()
 {
-	return &readyQueue.back();
+	int nextQueue = GetShortestQueue();
+	return &readyQueues[nextQueue].back();
+}
+
+PCB *Scheduler::Peek(int i)
+{
+	return &readyQueues[i].back();
 }
 
 PCB Scheduler::Dequeue()
 {
-	PCB result = readyQueue.back();
-	readyQueue.erase(readyQueue.end());
-	SortQueue(readyQueue, 0, readyQueue.size() - 1);
+	int nextQueue = GetShortestQueue();
+	PCB result = readyQueues[nextQueue].back();
+	readyQueues[nextQueue].erase(readyQueues[nextQueue].end());
+	SortQueue(readyQueues[nextQueue], 0, readyQueues[nextQueue].size() - 1);
+	return result;
+}
+
+PCB Scheduler::Dequeue(int i)
+{
+	PCB result = readyQueues[i].back();
+	readyQueues[i].erase(readyQueues[i].end());
+	SortQueue(readyQueues[i], 0, readyQueues[i].size() - 1);
 	return result;
 }
 
@@ -58,6 +83,21 @@ void Scheduler::Swap(PCB x, PCB y)
 	PCB tmp = x;
 	x = y;
 	y = tmp;
+}
+
+int Scheduler::GetShortestQueue()
+{
+	int shortest_index = 0;
+	int shortest = readyQueues[0].size();
+	for (int i = 0; i < readyQueues.size(); i++)
+	{
+		if (readyQueues[i].size() < shortest)
+		{
+			shortest = readyQueues[i].size();
+			shortest_index = i;
+		}
+	}
+	return shortest_index;
 }
 
 void Scheduler::SortQueue(std::vector<PCB> &toSort, int left, int right)
